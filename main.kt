805e6ff4031d57cfd6da4f139823d9bc2530622a
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2020-10-27 01:20:43 -0500

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var eq1 = "5 + 5 ="
+    var eq2 = "10 - 5 ="
+    var eq3 = "100 / 10 ="
+    var eq4 = "25 * 5 ="
+    var eq5 = "The remainder of 13 / 2 is"
+
+    var ans1 = 5 + 5
+    var ans2 = 10 - 5
+    var ans3 = 100 / 10
+    var ans4 = 25 * 5
+    var ans5 = 13 % 2
+
+    println("$eq1 $ans1")
+    println("$eq2 $ans2")
+    println("$eq3 $ans3")
+    println("$eq4 $ans4")
+    println("$eq5 $ans5")
+}
